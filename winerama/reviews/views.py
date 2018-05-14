ead82aa547326e46657131d1541fb8cfb69d3997
Jevin Subedi <jevinsubedi@gmail.com>
2018-05-14 00:25:11 -0500

    Added a ratings form inside wine detail page and html for wine_detail.

--- winerama/reviews/views.py
+++ winerama/reviews/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from .models import Wine, Review
+from .forms import ReviewForm
+import datetime
 
 # Create your views here.
 
@@ -21,3 +25,22 @@ def wine_detail(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
     return render(request, 'reviews/wine_detail.html', {'wine': wine})
 
+def add_review(request, wine_id):
+    wine = get_object_or_404(Wine, pk=wine_id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        rating = form.cleaned_data['rating']
+        comment = form.cleaned_data['comment']
+        user_name = form.cleaned_data['user_name']
+        review = Review()
+        review.wine = wine
+        review.user_name = user_name
+        review.rating = rating
+        review.comment = comment
+        review.pub_date = datetime.datetime.now()
+        review.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back Button.
+        return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id, )))
+    return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
